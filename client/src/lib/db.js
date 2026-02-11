0d77c379cc6764142722b2a974fe5bc4482e6c2b
lahmidi <dvabderrahman@gmail.com>
2026-02-11 15:22:25 +0100

    AP-2:start create authentication with google gmail and email.

--- client/src/lib/db.js
+++ client/src/lib/db.js
@@ -5,7 +5,7 @@ import * as schema from "./schema";
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
-    throw new Error("DATABASE_URL is not defined");
+    throw new Error("DATABASE_URL is not defined.");
 }
 
 const pool = new Pool({
